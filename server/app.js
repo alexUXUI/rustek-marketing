66eeda93fbc75b6c7917241f74fa21daef514829
Alex Bennett <alexandergraybennett@gmail.com>
2016-10-02 19:26:29 -0600

    adds procfile

--- server/app.js
+++ server/app.js
@@ -35,6 +35,10 @@ app.use('/', routes);
 app.use('/users', users);
 app.use('/signup', signup);
 
+app.get('/', (req, res, next) => {
+  res.send('hello!')
+})
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
