d8c0e716b0c0f1ee4a392a7d2244ff06c7b89c48
Alex Bennett <alexandergraybennett@gmail.com>
2016-10-16 15:08:16 -0600

    crankin away

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -9,29 +9,33 @@ import rustek from './rustek'
 import animation from './animations'
 import social from './social'
 
-// function *playWebsite() {
-//   yield setTimeout(animation.addDroppingSoon, 2000)
-//   yield setTimeout(animation.blur, 2000)
-//   yield setTimeout(animation.blur, 4000)
-//   yield setTimeout(animation.addSocialMedia, 5000)
-//   yield setTimeout(animation.addSignUp, 4600)
-//   yield setTimeout(rustek.init, 4600)
-// }
-
 function *playWebsite() {
-  yield setTimeout(animation.addDroppingSoon, 0)
-  yield setTimeout(animation.blur, 0)
-  yield setTimeout(animation.blur, 0)
-  yield setTimeout(animation.addSocialMedia, 0)
-  yield setTimeout(animation.addSignUp, 0)
-  yield setTimeout(rustek.init, 0)
+  yield setTimeout(animation.addDroppingSoon, 2000)
+  yield setTimeout(animation.blur, 2000)
+  // yield setTimeout(animation.blur, 4000)
+  yield setTimeout(animation.addSocialMedia, 5000)
+  yield setTimeout(animation.addSignUp, 4600)
+  yield setTimeout(rustek.init, 4600)
 }
+
+// function *playWebsite() {
+//   yield setTimeout(animation.addDroppingSoon, 0)
+//   yield setTimeout(animation.blur, 0)
+//   yield setTimeout(animation.blur, 0)
+//   yield setTimeout(animation.addSocialMedia, 0)
+//   yield setTimeout(animation.addSignUp, 0)
+//   yield setTimeout(rustek.init, 0)
+// }
 
 const player = playWebsite()
 
 for(let playerFunction of player) {
    player.next()
  }
+
+ $(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+});
 
 
 // invoke social
